0c6b45de128decf41ca241a3c68cfc9ced232bd0
Kamikazi-Deborah <deborahkamikazi9@gmail>
2024-08-26 15:33:02 +0200

    chore: configured colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+       Indigo:{
+        800:"#265E78",
+        400:"#3E7591",
+        200:"#64A1C0"
+       },
+       Gray:{
+        800:"#111827",
+        200:"#C9CDD3",
+        100:"#F9FAFB"
+       },
+       Mustard:{
+        200:"#EBF541",
+        100:"#F8FCAD",
+       },
+       Lachs:{
+        200: "#FFC28A"
+       }
+
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'lora': ['Lora', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
