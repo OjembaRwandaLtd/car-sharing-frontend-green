aae0a349ba2d512e635c2a9213f75f844d0c792a
Kamikazi-Deborah <deborahkamikazi9@gmail>
2024-08-27 14:20:39 +0200

    chore: implementing title component

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,13 @@ configure({
 
 function App(): ReactElement {
   return (
-    <main className="mx-auto flex min-h-screen w-1/3 flex-col gap-8 py-10">
-      <h1 className="text-4xl font-bold">Hello to MoniShare</h1>
-      <p>If you can read this, you have successfully started the base frontend repository!</p>
-      <p>Happy coding!</p>
+    <section className=" flex h-screen flex-col items-center gap-5  bg-primary">
       <Title />
-    </main>
+      <p className="font-lora text-xl text-secondary-light">
+        <span className="block px-10">Hello Manuela!</span>
+        <span className="block">What are you up to today?</span>
+      </p>
+    </section>
   )
 }
 
--- src/title.tsx
+++ src/title.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Title: React.FC = () => (
-  <div>
-    <p>MONI</p>
-    <p>Share</p>
+  <div className=" mt-8 text-secondary-light">
+    <p className="px-3 font-lora text-4xl font-bold">CAR</p>
+    <p className="font-lora text-3xl italic">Sharing</p>
   </div>
 )
 
