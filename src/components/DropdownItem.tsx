08d918de8724d678782a65b97a7f8ff5be7064d0
Angekarara <mbabange2020@gmail.com>
2024-08-27 16:13:25 +0200

    fix: dropdownitem

--- src/components/DropdownItem.tsx
+++ src/components/DropdownItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 interface Props {
   icon: React.ReactElement
@@ -8,10 +9,10 @@ interface Props {
 
 const DropdownItem = ({ icon, text, navlink }: Props): React.ReactElement => (
   <li className={`flex justify-between pb-0.5 pt-1 leading-3`}>
-    <a href={`${navlink ? navlink : ''}`}>
+    <NavLink to={`${navlink ? navlink : ''}`}>
       {icon}
       {text}
-    </a>
+    </NavLink>
   </li>
 )
 
