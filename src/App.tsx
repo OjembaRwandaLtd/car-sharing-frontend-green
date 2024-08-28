21819b3b942271adef928db9a246c4b7136a45ce
Kamikazi-Deborah <deborahkamikazi9@gmail>
2024-08-28 11:15:46 +0200

    feat: implementing homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from 'react'
 import { configure } from 'axios-hooks'
-import Title from './title'
+import Home from './home'
 // Configure axios hooks
 // Do not delete this if you want to use the provided API hooks in `src/hooks`
 configure({
@@ -10,15 +10,7 @@ configure({
 })
 
 function App(): ReactElement {
-  return (
-    <section className=" flex h-screen flex-col items-center gap-5  bg-primary">
-      <Title />
-      <p className="font-lora text-xl text-secondary-light">
-        <span className="block px-10">Hello Manuela!</span>
-        <span className="block">What are you up to today?</span>
-      </p>
-    </section>
-  )
+  return <Home />
 }
 
 export default App
--- src/title.tsx
+++ src/title.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Title: React.FC = () => (
-  <div className=" mt-8 text-secondary-light">
+  <div className=" mt-28 text-secondary-200">
     <p className="px-3 font-lora text-4xl font-bold">CAR</p>
-    <p className="font-lora text-3xl italic">Sharing</p>
+    <p className="font-lora text-3xl italic">sharing</p>
   </div>
 )
 
