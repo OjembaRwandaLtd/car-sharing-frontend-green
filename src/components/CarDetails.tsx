246af382c7fb153f395238ffc8eb5606c5b5e6f4
Chartine02 <noellachartine125@gmail.com>
2024-08-29 16:19:15 +0200

    feat: car detail implement (#10)

--- src/components/CarDetails.tsx
+++ src/components/CarDetails.tsx
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom"
 import CarIcon from "../assets/CarIcon"
 import FuelIcon from "../assets/FuelIcon"
 import HorseIcon from "../assets/HorseIcon"
@@ -6,12 +7,16 @@ import ProfileIcon from "../assets/ProfileIcon"
 import WarningIcon from "../assets/WarningIcon"
 import { useCarTypes, useCars } from "../hooks"
 import IconWithLabel from "./IconWithLabel"
+import Title from "./Title"
+import ErrorMessage from "./ErrorMessage"
 
 const CarDetails: React.FC = () => {
-  const [{ data: carTypeData, loading: carTypeLoading, error: carTypeError }] = useCarTypes()
-  const [{ data: carDetails }] = useCars()
+  const { carId } = useParams()
 
-  if (carTypeLoading)
+  const [{ data: carTypeData, loading: carTypeLoading }] = useCarTypes()
+  const [{ data: carDetails, loading: carLoading, error: carError }] = useCars()
+
+  if (carTypeLoading || carLoading)
     return (
       <div className="mx-auto -mt-20 flex h-screen scale-150 items-center justify-center text-white">
         <span className="loading loading-ring loading-xs"></span>
@@ -20,36 +25,45 @@ const CarDetails: React.FC = () => {
         <span className="loading loading-ring loading-lg"></span>
       </div>
     )
-  if (carTypeError) return <p>Error!</p>
+  if (carError) return <ErrorMessage text={"Error! Car Not found"} />
   return (
-    <div>
-      {carTypeData && (
-        <div className="flex h-screen flex-col">
-          <img className="-mt-20 scale-[1.05]" src={carTypeData[2].imageUrl} alt="My car" />
-          <div className="px-12 text-white">
-            <h2 className="-mt-16 py-8 font-lora text-2xl font-medium">{carTypeData[2].name}</h2>
-            {carDetails &&
-              carDetails
-                .filter(car => car.carTypeId === carTypeData[2].id)
-                .map(car => (
-                  <div key={car.id} className="">
-                    <IconWithLabel icon={<ProfileIcon />} text={`${car.name.split("'")[0]}`} />
-                    <IconWithLabel
-                      icon={<CarIcon />}
-                      text={`${carTypeData[2].name.split(" ")[1]}`}
-                    />
-                    {car.licensePlate && (
-                      <IconWithLabel icon={<LicensePlateIcon />} text={`${car.licensePlate}`} />
-                    )}
-                    {car.horsepower && (
-                      <IconWithLabel icon={<HorseIcon />} text={`${car.horsepower}hp`} />
-                    )}
-                    {car.fuelType && <IconWithLabel icon={<FuelIcon />} text={`${car.fuelType}`} />}
-                    {car.info && <IconWithLabel icon={<WarningIcon />} text={`${car.info}`} bold />}
-                  </div>
-                ))}
-          </div>
-        </div>
+    <div className="h-screen bg-primary-800">
+      <Title text="Details" backButton />
+      {carDetails && carDetails.filter(car => car.id === Number(carId)).length > 0 ? (
+        carDetails
+          .filter(car => car.id === Number(carId))
+          .map(car => (
+            <div key={car.id} className="flex h-screen flex-col">
+              <img
+                className="-mt-20 scale-[1.05]"
+                src={
+                  carTypeData
+                    ? carTypeData.filter(item => item.id === car.carTypeId)[0].imageUrl
+                    : ""
+                }
+                alt="My car"
+              />
+              <div className="px-12 text-white">
+                <h2 className="-mt-16 py-8 font-lora text-2xl font-medium">
+                  {carTypeData ? carTypeData.filter(item => item.id === car.carTypeId)[0].name : ""}
+                </h2>
+                <div>
+                  <IconWithLabel icon={<ProfileIcon />} text={`${car.name.split("'")[0]}`} />
+                  <IconWithLabel icon={<CarIcon />} text={`${car.name.split(" ")[1]}`} />
+                  {car.licensePlate && (
+                    <IconWithLabel icon={<LicensePlateIcon />} text={`${car.licensePlate}`} />
+                  )}
+                  {car.horsepower && (
+                    <IconWithLabel icon={<HorseIcon />} text={`${car.horsepower}hp`} />
+                  )}
+                  {car.fuelType && <IconWithLabel icon={<FuelIcon />} text={`${car.fuelType}`} />}
+                  {car.info && <IconWithLabel icon={<WarningIcon />} text={`${car.info}`} bold />}
+                </div>
+              </div>
+            </div>
+          ))
+      ) : (
+        <ErrorMessage text={"Error! Car Not found"} />
       )}
     </div>
   )
