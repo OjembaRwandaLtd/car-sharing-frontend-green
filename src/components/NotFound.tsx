2da09bf017ee94b6455930b49171e26d414e63fc
Chartine02 <noellachartine125@gmail.com>
2024-08-29 17:41:51 +0200

    feat: error page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,14 +1,16 @@
 import React from "react"
 import CarError from "../assets/CarError"
+import Button from "./Button"
 
 const NotFound: React.FC = (): React.ReactElement => (
-  <div className="bg-primary-DEFAUL text-secondary-light flex h-screen flex-col items-center gap-8 py-24 font-lora">
+  <div className="flex h-screen flex-col items-center gap-8 bg-primary-800 py-24 font-lora text-white">
     <h1 className="text-5xl font-bold">OOOOOPS!</h1>
     <CarError />
-    <p className="flex flex-col text-center text-lg font-medium">
+    <p className="flex flex-col pb-8 text-center text-lg font-medium">
       Something went wrong.
       <span>We will solve your issue soon.</span>
     </p>
+    <Button value="Go Back" />
   </div>
 )
 
