3fc5d081db9692ee35bcbc3d7339160fd2020b35
Kamikazi-Deborah <deborahkamikazi9@gmail>
2024-08-30 10:17:18 +0200

    feat: home page implemented

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,24 @@
 import React from "react"
 import Title from "./Title"
+import Navbar from "./Navbar"
+import Button from "./Button"
 const Home: React.FC = () => (
-  <section className=" bg-primary-800 flex h-screen flex-col items-center gap-5">
-    <Title />
-    <p className="font-lora text-secondary-200 text-xl">
-      <span className="block px-10">Hello Manuela!</span>
-      <span className="block">What are you up to today?</span>
-    </p>
-  </section>
+  <div className="h-screen bg-primary-800">
+    <Navbar />
+    <section className=" flex  flex-col items-center gap-10">
+      <Title />
+      <p className="font-lora text-xl text-secondary-200 ">
+        <span className="block px-10">Hello Manuela!</span>
+        <span className="block">What are you up to today?</span>
+      </p>
+      <Button value="Book car" />
+      <p className="font-lora text-xl text-secondary-200">or</p>
+    </section>
+    <div className="mt-7 flex flex-col gap-4">
+      <Button value="See My Cars" type="outline" />
+      <Button value="See My Bookings" type="outline" />
+    </div>
+  </div>
 )
 
 export default Home
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,9 @@
 import React from "react"
 
 const Title: React.FC = () => (
-  <div className=" text-secondary-200 mt-28">
-    <p className="font-lora px-3 text-4xl font-bold">CAR</p>
-    <p className="font-lora text-3xl italic">sharing</p>
+  <div className=" mt-16 text-secondary-200">
+    <p className="px-7 font-lora text-4xl font-bold">CAR</p>
+    <p className="font-lora text-4xl italic">sharing</p>
   </div>
 )
 
