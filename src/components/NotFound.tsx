d69cec5c5fa03b1dcb53d76abf15a104157cdeb8
Chartine02 <noellachartine125@gmail.com>
2024-08-30 12:47:23 +0200

    fix: merge changes from dev

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,17 +1,23 @@
 import { ReactElement } from "react"
 import CarError from "../assets/CarError"
 import Button from "./Button"
+import { useNavigate } from "react-router-dom"
 
-const NotFound = (): ReactElement => (
-  <div className="flex h-screen flex-col items-center gap-8 bg-primary-800 py-20 font-lora text-white">
-    <h1 className="text-5xl font-bold">OOOOOPS!</h1>
-    <CarError />
-    <p className="flex flex-col pb-8 text-center text-lg font-medium">
-      Something went wrong.
-      <span>We will solve your issue soon.</span>
-    </p>
-    <Button value="Go Back" />
-  </div>
-)
+const NotFound = (): ReactElement => {
+  const navigate = useNavigate()
+  const handleClick = () => navigate(-1)
+
+  return (
+    <div className="flex h-screen flex-col items-center gap-8 bg-primary-800 py-20 font-lora text-white">
+      <h1 className="text-5xl font-bold">OOOOOPS!</h1>
+      <CarError />
+      <p className="flex flex-col pb-8 text-center text-lg font-medium">
+        Something went wrong.
+        <span>We will solve your issue soon.</span>
+      </p>
+      <Button value="Go Back" handleClick={handleClick} />
+    </div>
+  )
+}
 
 export default NotFound
