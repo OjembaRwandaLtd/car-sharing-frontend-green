aa23e34a3bec60f85d7eb0c80a4d167dc25d4bf4
Chartine02 <noellachartine125@gmail.com>
2024-09-02 17:46:29 +0200

    refactor: prop naming

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 import daisyui from "daisyui"
 
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
 
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
