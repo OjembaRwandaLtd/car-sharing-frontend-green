5c59a268e2230e4fe928d3773b4049d4cb8247b9
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-09-04 14:31:23 +0200

    chore: refactored IconWithLabel file

--- src/assets/IconWithLabel.tsx
+++ src/assets/IconWithLabel.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames"
 import React from "react"
 
 interface Prop {
@@ -5,11 +6,10 @@ interface Prop {
   text: string
   bold?: boolean
 }
-
 const IconWithLabel = ({ icon, text, bold }: Prop): React.ReactElement => (
   <div className="flex items-center gap-2 pb-2 font-inter text-sm font-normal">
     {icon}
-    <p className={`text-lg ${bold ? "font-bold" : ""}`}>{text}</p>
+    <p className={classNames("text-lg", { "font-bold": bold })}>{text}</p>
   </div>
 )
 
