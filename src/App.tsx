18fc3492a8f7714612295dff4fa58ada36301615
Eliezer Nsengi <avaranstudios@gmail.com>
2024-09-04 18:20:02 +0200

    fix: fixed the whole project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { ReactElement } from 'react'
-import { configure } from 'axios-hooks'
+import { ReactElement } from "react"
+import { configure } from "axios-hooks"
 
 // Configure axios hooks
 // Do not delete this if you want to use the provided API hooks in `src/hooks`
