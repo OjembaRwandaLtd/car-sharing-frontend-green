b2cf8a0dc1abcf9621ca36a1d4e63f299fde59a2
Eliezer Nsengi <avaranstudios@gmail.com>
2024-09-04 11:38:50 +0200

    fix: use navigate hook title component

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -5,21 +5,25 @@ interface Props {
   text: string
   backButton?: boolean
 }
-const navigate = useNavigate()
 
-const Title = ({ text, backButton }: Props): React.ReactElement => (
-  <div className="flex items-center px-4 py-8">
-    {backButton && (
-      <button
-        className="z-20 mr-4 h-6 w-6 cursor-pointer text-2xl outline-none"
-        onClick={() => navigate(-1)}
-        aria-label="Go back"
-      >
-        <ChevronBackIcon className="h-full w-full text-mustard-200" />
-      </button>
-    )}
-    <h1 className="grow text-center font-lora text-3xl font-medium uppercase text-white">{text}</h1>
-  </div>
-)
+const Title = ({ text, backButton }: Props): React.ReactElement => {
+  const navigate = useNavigate()
+  return (
+    <div className="flex items-center px-4 py-8">
+      {backButton && (
+        <button
+          className="z-20 mr-4 h-6 w-6 cursor-pointer text-2xl outline-none"
+          onClick={() => navigate(-1)}
+          aria-label="Go back"
+        >
+          <ChevronBackIcon className="h-full w-full text-mustard-200" />
+        </button>
+      )}
+      <h1 className="grow text-center font-lora text-3xl font-medium uppercase text-white">
+        {text}
+      </h1>
+    </div>
+  )
+}
 
 export default Title
