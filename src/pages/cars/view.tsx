ead6972d9179314be795f717a9f4a447aae0f693
Chartine02 <noellachartine125@gmail.com>
2024-09-05 12:47:49 +0200

    feat: responsive car details

--- src/pages/cars/view.tsx
+++ src/pages/cars/view.tsx
@@ -27,14 +27,20 @@ const CarDetails = (): ReactElement => {
   return (
     <>
       {car && car.length > 0 ? (
-        <div className="overflow-x-hidden bg-primary-800">
+        <div className="overflow-x-hidden bg-primary-800 lg:-mt-32 lg:overflow-y-hidden">
           <Title text="Details" backButton />
           {car.map(car => (
-            <div key={car.id} className="flex flex-col">
-              <img className="-mt-20 scale-[1.05]" src={car.carImage ?? ""} alt="My car" />
-              <div className="px-12 text-white">
-                <h2 className="-mt-16 py-8 font-lora text-2xl font-medium">{car.carName ?? ""}</h2>
-                <div>
+            <div key={car.id} className="flex flex-col  md:flex-row md:items-center md:text-center">
+              <img
+                className="md:-py-0 -mt-20 scale-[1.05]  md:-mt-56 md:scale-75"
+                src={car.carImage ?? ""}
+                alt="My car"
+              />
+              <div className="px-12 text-white lg:-mt-40 lg:bg-primary-400 lg:p-12">
+                <h2 className="-mt-16 py-8 font-lora text-2xl font-medium md:text-5xl ">
+                  {car.carName ?? ""}
+                </h2>
+                <div className="md:text-3xl">
                   <IconWithLabel icon={<ProfileIcon />} text={car.carOwner} />
                   <IconWithLabel icon={<CarIcon />} text={car.carName} />
                   {car.licensePlate && (
