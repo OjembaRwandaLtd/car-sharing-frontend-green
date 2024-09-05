1d34571ffcf0aacabd5172616a023dcbcd9886a4
Kamikazi-Deborah <deborahkamikazi9@gmail>
2024-09-05 15:46:33 +0200

    chore: car listing

---
+++ src/components/cards/Car/Item.tsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom"
+import { CarIcon, IconWithLabel, ProfileIcon } from "../../../assets"
+import Button from "../../ui/Button"
+
+interface Props {
+  car: {
+    id: number
+    carImage: string
+    carName: string
+    carOwner: string
+  }
+  deleteButton?: boolean
+  bookButton?: boolean
+}
+
+const Item = ({ car, deleteButton, bookButton }: Props) => (
+  <div key={car.id} className="mx-auto my-4 w-11/12 rounded-xl bg-primary-400 py-4">
+    <div className="flex h-64 gap-2">
+      <div className="w-72">
+        {car && (
+          <img src={car.carImage} className="h-full w-full rotate-3 scale-105" alt={car.carName} />
+        )}
+      </div>
+      <div className="mr-7 mt-4 border-spacing-y-4 space-y-4 text-secondary-200">
+        <h2 className="font-lora text-xl font-medium">{car.carName}</h2>
+        <div className="space-y-2 truncate">
+          <IconWithLabel icon={<ProfileIcon />} text={car.carOwner} />
+          <IconWithLabel icon={<CarIcon />} text={car.carName.split(" ")[1] || ""} />
+          <div className="pt-8">
+            <Link to={`${car.id}`} className="mt-7 font-inter text-base font-bold text-mustard-200">
+              Show details
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+    {deleteButton && <Button value="Delete Car" type="outlineLachs" />}
+    {bookButton && <Button value="Book Car" />}
+  </div>
+)
+
+export default Item
---
+++ src/components/cards/Car/index.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from "react"
+import Item from "./Item"
+import Loading from "../../ui/Loading"
+import NotFound from "../../../pages/404"
+import { useCarDetails } from "../../../hooks"
+
+const Car = (): ReactElement => {
+  const { carsData, isLoading, isError } = useCarDetails()
+
+  if (isLoading) return <Loading />
+  if (isError) return <NotFound />
+  return <div>{carsData?.map(car => <Item key={car.id} car={car} deleteButton />)}</div>
+}
+
+export default Car
