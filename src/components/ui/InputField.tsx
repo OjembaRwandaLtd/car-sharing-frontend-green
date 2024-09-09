f1710b834d8fa197ff429f63a99a4794bcdaf696
Kamikazi-Deborah <deborahkamikazi9@gmail>
2024-09-09 15:06:05 +0200

    feat: implementation of offer own car

---
+++ src/components/ui/InputField.tsx
@@ -0,0 +1,66 @@
+import { ReactElement, useState } from "react"
+import { ChevronDownIcon } from "../../assets"
+
+interface Props {
+  title?: string
+  icon?: ReactElement
+  placeholder: string
+  dropdownData?: string[]
+}
+const InputField = ({ title, icon, placeholder, dropdownData }: Props): ReactElement => {
+  const [input, setInput] = useState("")
+  const [showDropdown, setShowDropdown] = useState(true)
+
+  return (
+    <div className="w-full">
+      <div className="label">
+        {title && (
+          <label className="label-text font-inter text-sm text-white" htmlFor={title}>
+            {title}
+          </label>
+        )}
+      </div>
+      <div className="input flex items-center gap-2 rounded-full bg-primary-200 px-5 py-6 ">
+        {icon}
+        <input
+          onChange={e => setInput(e.target.value)}
+          value={input}
+          type="text"
+          placeholder={placeholder}
+          className="-mt-1 h-14 w-full text-white placeholder:text-white"
+        />
+
+        {dropdownData && (
+          <div className="dropdown-end dropdown text-white">
+            <div tabIndex={0} role="button" className="mr-1">
+              <div onClick={() => setShowDropdown(!showDropdown)}>
+                <ChevronDownIcon className="scale-150" />
+              </div>
+              {showDropdown && (
+                <ul
+                  className="menu dropdown-content z-[1] mt-1
+               w-52 gap-1 divide-y-[1px] rounded-box bg-primary-400 p-2"
+                  tabIndex={0}
+                >
+                  {dropdownData.map(item => (
+                    <li
+                      key={item}
+                      onClick={() => {
+                        setInput(item)
+                        setShowDropdown(!showDropdown)
+                      }}
+                    >
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default InputField
--- src/pages/cars/new.tsx
+++ src/pages/cars/new.tsx
@@ -1,5 +1,23 @@
 import { ReactElement } from "react"
+import Title from "../../components/ui/Title"
+import InputField from "../../components/ui/InputField"
+import Button from "../../components/ui/Button"
 
-const AddNewCar: React.FC = (): ReactElement => <div>Add new a car here!</div>
-
+const AddNewCar: React.FC = (): ReactElement => (
+  <section className="flex flex-col items-center px-3 pb-5">
+    <Title text="new car" />
+    <InputField title={"Name"} placeholder={"e.g. My Nice Moni Car"} />
+    <InputField title={"Type"} placeholder={"Moni Cooper"} dropdownData={["kami", "debora"]} />
+    <div className="flex gap-1">
+      <InputField title={"License Plate"} placeholder={"e.g. M-XY 123"} />
+      <InputField title={"Horse Power"} placeholder={"110"} />
+    </div>
+    <InputField title={"Fuel type"} placeholder={"e.g. 150"} dropdownData={["kami", "debora"]} />
+    <InputField title={"Additional Information"} placeholder={"e.g. No smoking"} />
+    <div className="mt-32 flex gap-1">
+      <Button width="regular" value="Cancel" type="outline" />
+      <Button width="regular" value="Add Car" />
+    </div>
+  </section>
+)
 export default AddNewCar
