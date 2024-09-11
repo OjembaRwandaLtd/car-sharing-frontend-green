2f37bbbd6a826db47391d83b48890e31696f07d4
Angekarara <mbabange2020@gmail.com>
2024-09-11 15:14:41 +0200

    chore: implemented delete own car

--- src/components/cards/Car/Item.tsx
+++ src/components/cards/Car/Item.tsx
@@ -2,9 +2,6 @@ import { ReactElement } from "react"
 import { Link } from "react-router-dom"
 import { CarIcon, IconWithLabel, ProfileIcon } from "../../../assets"
 import Button from "../../ui/Button"
-import axios from "axios"
-import { apiUrl } from "../../../util/apiUrl"
-import { getAuthToken } from "../../../util/auth"
 
 interface Props {
   car: {
@@ -15,54 +12,33 @@ interface Props {
   }
   deleteButton?: boolean
   bookButton?: boolean
+  handleDelete?: () => void
 }
 
-const Item = ({ car, deleteButton, bookButton }: Props): ReactElement => {
-  const handleDelete = async () => {
-    try {
-      // const response =
-      await axios.delete(`${apiUrl}/cars/${car.id}`, {
-        headers: {
-          Authorization: `Bearer ${getAuthToken()}`,
-        },
-      })
-      // console.log(response.data)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-  return (
-    <div key={car.id} className="mx-auto my-4 w-11/12 rounded-xl bg-primary-400 py-4">
-      <div className="flex h-64 gap-2">
-        <div className="w-72">
-          {car && (
-            <img
-              src={car.carImage}
-              className="h-full w-full rotate-3 scale-105"
-              alt={car.carName}
-            />
-          )}
-        </div>
-        <div className="mr-7 mt-4 border-spacing-y-4 space-y-4 text-secondary-200">
-          <h2 className="font-lora text-xl font-medium">{car.carName}</h2>
-          <div className="space-y-2 truncate">
-            <IconWithLabel icon={<ProfileIcon />} text={car.carOwner} />
-            <IconWithLabel icon={<CarIcon />} text={car.carName.split(" ")[1] || ""} />
-            <div className="pt-8">
-              <Link
-                to={`${car.id}`}
-                className="mt-7 font-inter text-base font-bold text-mustard-200"
-              >
-                Show details
-              </Link>
-            </div>
+const Item = ({ car, deleteButton, bookButton, handleDelete }: Props): ReactElement => (
+  <div key={car.id} className="mx-auto my-4 w-11/12 rounded-xl bg-primary-400 py-4">
+    <div className="flex h-64 gap-2">
+      <div className="w-72">
+        {car && (
+          <img src={car.carImage} className="h-full w-full rotate-3 scale-105" alt={car.carName} />
+        )}
+      </div>
+      <div className="mr-7 mt-4 border-spacing-y-4 space-y-4 text-secondary-200">
+        <h2 className="font-lora text-xl font-medium">{car.carName}</h2>
+        <div className="space-y-2">
+          <IconWithLabel icon={<ProfileIcon />} text={car.carOwner} />
+          <IconWithLabel icon={<CarIcon />} text={car.carName.split(" ")[1] || ""} />
+          <div className="pt-8">
+            <Link to={`${car.id}`} className="mt-7 font-inter text-base font-bold text-mustard-200">
+              Show details
+            </Link>
           </div>
         </div>
       </div>
-      {deleteButton && <Button value="Delete Car" type="outlineLachs" handleClick={handleDelete} />}
-      {bookButton && <Button value="Book Car" />}
     </div>
-  )
-}
+    {deleteButton && <Button value="Delete Car" type="outlineLachs" handleClick={handleDelete} />}
+    {bookButton && <Button value="Book Car" />}
+  </div>
+)
 
 export default Item
--- src/pages/cars/own.tsx
+++ src/pages/cars/own.tsx
@@ -1,13 +1,37 @@
-import { ReactElement } from "react"
+import { ReactElement, useEffect, useState } from "react"
 import Item from "../../components/cards/Car/Item"
 import Button from "../../components/ui/Button"
 import Loading from "../../components/ui/Loading"
 import NotFound from "../404"
 import { useCarDetails } from "../../hooks"
+import { getAuthToken } from "../../util/auth"
+import { apiUrl } from "../../util/apiUrl"
+import axios from "axios"
 
 const OwnCars = (): ReactElement => {
   const { carsData, isLoading, isError } = useCarDetails()
   const userId = localStorage.getItem("userId")
+
+  const [cars, setCars] = useState([])
+
+  useEffect(() => {
+    if (carsData) {
+      setCars(carsData.filter(car => car.ownerId === Number(userId)))
+    }
+  }, [carsData, userId])
+
+  const handleDelete = async (carId: number) => {
+    try {
+      await axios.delete(`${apiUrl}/cars/${carId}`, {
+        headers: {
+          Authorization: `Bearer ${getAuthToken()}`,
+        },
+      })
+      setCars(prevCars => prevCars.filter(car => car.id !== carId))
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   if (isLoading) return <Loading />
   if (isError) return <NotFound />
@@ -15,9 +39,9 @@ const OwnCars = (): ReactElement => {
   return (
     <div>
       <div>
-        {carsData
-          ?.filter(car => car.ownerId === Number(userId))
-          .map(car => <Item key={car.id} car={car} deleteButton />)}
+        {cars?.map(car => (
+          <Item key={car.id} car={car} deleteButton handleDelete={() => handleDelete(car.id)} />
+        ))}
       </div>
       <Button value="Add Car" />
     </div>
