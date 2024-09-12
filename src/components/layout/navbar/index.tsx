3af6213da08a0bed324350da38dddb3a82efd25c
Kamikazi-Deborah <deborahkamikazi9@gmail>
2024-09-12 18:10:33 +0200

    fix: dropdown data

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -2,7 +2,7 @@ import { ReactElement, useEffect, useRef, useState } from "react"
 import classNames from "classnames"
 import { ProfileIcon, Logo } from "../../../assets/index"
 import { NavLink, useLocation } from "react-router-dom"
-import Dropdown from "../../ui/dropdown"
+import Dropdown from "../../ui/Dropdown"
 import DropdownItems from "./DropdownItems"
 
 const Navbar = (): ReactElement => {
