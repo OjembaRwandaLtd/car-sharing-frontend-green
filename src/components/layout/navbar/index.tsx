6611dd7d144e5a132e5c67ce5177dd4f4a0905e3
Chartine02 <noellachartine125@gmail.com>
2024-09-16 14:04:00 +0200

    chore: hide menu and profile before logging in

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -4,12 +4,14 @@ import { ProfileIcon, Logo } from "../../../assets/index"
 import { NavLink, useLocation } from "react-router-dom"
 import Dropdown from "../../ui/Dropdown"
 import DropdownItems from "./DropdownItems"
+import { getAuthToken } from "../../../util/auth"
 
 const Navbar = (): ReactElement => {
   const [isOpen, setIsOpen] = useState(false)
   const dropdownRef = useRef<HTMLDivElement>(null)
   const toggleDropdown = () => setIsOpen(!isOpen)
   const location = useLocation()
+  const token = getAuthToken()
 
   const handleClickOutside = (event: MouseEvent) => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) setIsOpen(false)
@@ -25,26 +27,28 @@ const Navbar = (): ReactElement => {
   return (
     <div>
       <nav className="relative flex h-14 items-center justify-between rounded-b-xl bg-secondary-800 px-5 py-4 text-white">
-        <div
-          ref={dropdownRef}
-          className={classNames("dropdown dropdown-bottom", { isOpen: "dropdown-open" })}
-        >
-          <div tabIndex={0} role="button" className="m-1" onClick={toggleDropdown}>
-            {isOpen ? "Close" : "Menu"}
-          </div>
+        {token && (
+          <div
+            ref={dropdownRef}
+            className={classNames("dropdown dropdown-bottom", { isOpen: "dropdown-open" })}
+          >
+            <div tabIndex={0} role="button" className="m-1" onClick={toggleDropdown}>
+              {isOpen ? "Close" : "Menu"}
+            </div>
 
-          {isOpen && (
-            <Dropdown>
-              <DropdownItems />
-            </Dropdown>
-          )}
-        </div>
+            {isOpen && (
+              <Dropdown>
+                <DropdownItems />
+              </Dropdown>
+            )}
+          </div>
+        )}
         <div className="absolute left-1/2 top-0 z-0 -translate-x-1/2 rounded-full bg-secondary-800 px-4 pb-5 pt-1.5">
           <NavLink to={"/"}>
             <Logo className="size-10 z-10" />
           </NavLink>
         </div>
-        <ProfileIcon className="w-6" />
+        {token && <ProfileIcon className="w-6" />}
       </nav>
     </div>
   )
