26a679d3fc2c8c6929be39348063d03765f16f8d
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-09-16 13:35:05 +0200

    refactor: refactoring new car form

--- src/components/forms/NewCar/index.tsx
+++ src/components/forms/NewCar/index.tsx
@@ -1,13 +1,11 @@
-import { ToastContainer } from "react-toastify"
-import Title from "../../ui/Title"
 import { INITIAL_FORM_VALUES, formFieldsData } from "../../../util/newCarData"
 import InputField from "../../ui/InputField"
-import Button from "../../ui/Button"
 import { useCarDetails, useCarTypes } from "../../../hooks"
 import Loading from "../../ui/Loading"
 import NotFound from "../../../pages/404"
 import { NewCarProps } from "../../../util/props/newCar"
 import { useMemo } from "react"
+import NewCarFormWrapper from "./Wrapper"
 
 type CarFieldNames = "type" | "fuel_type"
 const NewCarForm = ({
@@ -23,7 +21,7 @@ const NewCarForm = ({
   const dropdownData: { type: string[]; fuel_type: string[] } = useMemo(
     () => ({
       type: carTypes[0]?.data?.map(type => type.name) || [""],
-      fuel_type: [...(new Set(carsData?.map(({ fuelType }) => fuelType)) || [])],
+      fuel_type: [...new Set(carsData?.map(({ fuelType }) => fuelType))],
     }),
     [carsData, carTypes],
   )
@@ -31,39 +29,26 @@ const NewCarForm = ({
   if (isLoading) return <Loading />
   if (isError) return <NotFound />
   return (
-    <form className="px-3 pb-5 md:mx-32 lg:mx-40" autoComplete="off">
-      <ToastContainer theme="colored" />
-      <Title text="New Car" />
-      <div className="grid grid-cols-2 space-y-3">
-        {formFieldsData.map(el => (
-          <InputField
-            key={el.title}
-            type={el.type}
-            span={el.span}
-            title={el.title}
-            name={el.name}
-            value={form[el.name]}
-            onChange={handleFormDataChange}
-            setForm={setForm}
-            placeholder={el.placeholder}
-            dropdownData={dropdownData[el.name as CarFieldNames]}
-          />
-        ))}
-      </div>
-      <div className="mx-auto mt-24 flex w-fit gap-1">
-        <Button
-          width="regular"
-          value="Cancel"
-          type="outline"
-          handleClick={() => setForm(INITIAL_FORM_VALUES)}
+    <NewCarFormWrapper
+      onCancel={() => setForm(INITIAL_FORM_VALUES)}
+      saveButtonText={isSubmitting ? "Adding Car..." : "Add Car"}
+      onSubmit={() => handleSubmit()}
+    >
+      {formFieldsData.map(el => (
+        <InputField
+          key={el.title}
+          type={el.type}
+          span={el.span}
+          title={el.title}
+          name={el.name}
+          value={form[el.name]}
+          onChange={handleFormDataChange}
+          setForm={setForm}
+          placeholder={el.placeholder}
+          dropdownData={dropdownData[el.name as CarFieldNames]}
         />
-        <Button
-          width="regular"
-          value={isSubmitting ? "Adding Car..." : "Add Car"}
-          handleClick={() => handleSubmit()}
-        />
-      </div>
-    </form>
+      ))}
+    </NewCarFormWrapper>
   )
 }
 
