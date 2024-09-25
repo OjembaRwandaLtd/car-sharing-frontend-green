96b139f78ac63e0242a231a7990094ada311c5d9
Chartine02 <noellachartine125@gmail.com>
2024-09-25 12:05:46 +0200

    chore: implemented displaying calendar on top of the inputs

--- src/components/forms/BookCar.tsx
+++ src/components/forms/BookCar.tsx
@@ -1,43 +1,68 @@
+import { useState } from "react"
 import Button from "../ui/Button"
 import Calendar from "../ui/Calendar"
 import Title from "../ui/Title"
 
-const BookCar = () => (
-  <section>
-    <Title text="BOOK CAR" />
-    <form className=" mt-20 flex flex-col justify-center gap-2">
-      <label htmlFor="start date" className="font-inter text-sm text-white">
-        Start date
-      </label>
-      <div className="input">
-        <input
-          type="text"
-          id="username"
-          placeholder="06/07/2023 01:07 PM"
-          //   value={formData.username}
-          className="w-72"
-          name="username"
-          //   onChange={handleChange}
-        />
-      </div>
-      <label className="font-inter text-sm text-white"> End date</label>
-      <div className="input">
-        <input
-          type="text"
-          id="username"
-          placeholder="06/07/2023 01:07 PM"
-          //   value={formData.username}
-          className="w-72"
-          name="username"
-          //   onChange={handleChange}s
-        />
-      </div>
-      <div className="mt-20">
-        <Button value=" Search Available Cars" />
-      </div>
-    </form>
-    <Calendar />
-  </section>
-)
+// const INITIAL_VALUES = {
+//   startTime: dayjs()?.$d,
+//   endTime: dayjs()?.$d,
+// }
+
+const BookCar = () => {
+  const [showCalendar, setShowCalendar] = useState(false)
+  // const [inputValue, setInputValue] = useState(INITIAL_VALUES)
+
+  const handleClick = () => {
+    setShowCalendar(prev => !prev)
+  }
+  // const time = useContext(TimeContext)
+  // console.log(dayjs().$d)
+
+  return (
+    <>
+      {showCalendar ? (
+        <Calendar setShowCalendar={setShowCalendar} />
+      ) : (
+        <section>
+          <Title text="BOOK CAR" />
+          <form className=" mt-20 flex flex-col justify-center gap-2">
+            <label htmlFor="start date" className="font-inter text-sm text-white">
+              Start date
+            </label>
+            <div className="input">
+              <input
+                type="text"
+                id="start-date"
+                placeholder="06/07/2023 01:07 PM"
+                // value={inputValue}
+                className="w-72"
+                name="start-date"
+                //   onChange={handleChange}
+                onClick={handleClick}
+              />
+            </div>
+            {/* {showCalendar && <Calendar />} */}
+            <label className="font-inter text-sm text-white"> End date</label>
+            <div className="input">
+              <input
+                type="text"
+                id="end-date"
+                placeholder="06/07/2023 01:07 PM"
+                //   value={formData.username}
+                className="w-72"
+                name="end-date"
+                onClick={handleClick}
+                //   onChange={handleChange}s
+              />
+            </div>
+            <div className="mt-20">
+              <Button value=" Search Available Cars" />
+            </div>
+          </form>
+        </section>
+      )}
+    </>
+  )
+}
 
 export default BookCar
--- src/components/ui/Calendar.tsx
+++ src/components/ui/Calendar.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react"
+import { createContext, ReactElement, useState } from "react"
 import { createTheme, ThemeProvider } from "@mui/material/styles"
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
 import { StaticDateTimePicker } from "@mui/x-date-pickers/StaticDateTimePicker"
 import dayjs, { Dayjs } from "dayjs"
+import Button from "@mui/material/Button"
 
 const theme = createTheme({
   palette: {
@@ -14,20 +15,54 @@ const theme = createTheme({
       main: "#dc004e",
     },
   },
-  // Add more theme customizations as needed
 })
 
-const App: React.FC = () => {
-  const [value, setValue] = useState<Dayjs | null>(dayjs())
-  console.log(value)
+interface Props {
+  setShowCalendar: (e: boolean) => void
+}
 
+export const TimeContext = createContext<Dayjs | null>(dayjs())
+
+const Calendar = ({ setShowCalendar }: Props): ReactElement => {
+  const [value, setValue] = useState<Dayjs | null>(dayjs())
+  const [tempValue, setTempValue] = useState<Dayjs | null>(dayjs())
+
+  // console.log(value.$d.toString())
+
+  const handleOkClick = () => {
+    setValue(tempValue)
+    setShowCalendar(false)
+  }
+
+  const handleCancelClick = () => {
+    setTempValue(value)
+    setShowCalendar(false)
+  }
+
+  const CustomBar = () => (
+    <div className=" absolute bottom-0 right-0">
+      <Button onClick={handleCancelClick}>Cancel</Button>
+      <Button onClick={handleOkClick}>OK</Button>
+      {/* Customize other buttons (Clear, Today) if needed */}
+    </div>
+  )
   return (
-    <ThemeProvider theme={theme}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <StaticDateTimePicker value={value} onChange={newValue => setValue(newValue)} />
-      </LocalizationProvider>
-    </ThemeProvider>
+    <TimeContext.Provider value={tempValue}>
+      <div className="relative mt-4">
+        <ThemeProvider theme={theme}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <StaticDateTimePicker
+              value={value}
+              onChange={(newValue: Dayjs | null) => setValue(newValue)}
+              slots={{
+                actionBar: CustomBar, // Use the "slots" prop to inject custom ActionBar
+              }}
+            />
+          </LocalizationProvider>
+        </ThemeProvider>
+      </div>
+    </TimeContext.Provider>
   )
 }
 
-export default App
+export default Calendar
