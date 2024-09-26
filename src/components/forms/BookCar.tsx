dc7570e990dd0cfd0ab5d4d6fdcd52fa3f1c11b3
Eliezer Nsengi <avaranstudios@gmail.com>
2024-09-26 11:10:52 +0200

    fix: calendar input

--- src/components/forms/BookCar.tsx
+++ src/components/forms/BookCar.tsx
@@ -1,82 +1,18 @@
-import { useState } from "react"
 import Button from "../ui/Button"
 import Calendar from "../ui/Calendar"
 import Title from "../ui/Title"
-import { useTime } from "../../hooks/useCalendarContext"
-import dayjs, { Dayjs } from "dayjs"
 
-const BookCar = () => {
-  const { time } = useTime()
-  const [showCalendar, setShowCalendar] = useState(false)
-  const [currentTime, setCurrentTime] = useState(time)
-  const [startDate, setStartDate] = useState(time)
-  const [endDate, setEndDate] = useState(time)
-  const [isSettingStartDate, setIsSettingStartDate] = useState(true)
-
-  const handleClick = (isStart: boolean) => {
-    setIsSettingStartDate(isStart)
-    setShowCalendar(true)
-  }
-
-  const handleCalendarClose = (selectedTime: Dayjs | null) => {
-    if (isSettingStartDate) setStartDate(selectedTime)
-    else setEndDate(selectedTime)
-    setShowCalendar(false)
-  }
-
-  const formatDate = (date: Dayjs | null) => dayjs(date).format("YYYY-MM-DD HH:mm")
-
-  return (
-    <>
-      {showCalendar ? (
-        <Calendar
-          setShowCalendar={setShowCalendar}
-          currentTime={currentTime}
-          setCurrentTime={setCurrentTime}
-          onClose={handleCalendarClose}
-        />
-      ) : (
-        <section>
-          <Title text="BOOK CAR" />
-          <form className="mt-20 flex flex-col justify-center gap-2">
-            <label htmlFor="start-date" className="font-inter text-sm text-white">
-              Start date
-            </label>
-            <div className="input">
-              <input
-                type="text"
-                id="start-date"
-                placeholder="YYYY-MM-DD HH:mm"
-                className="w-72"
-                name="startTime"
-                value={formatDate(startDate)}
-                onClick={() => handleClick(true)}
-                readOnly
-              />
-            </div>
-            <label htmlFor="end-date" className="font-inter text-sm text-white">
-              End date
-            </label>
-            <div className="input">
-              <input
-                type="text"
-                id="end-date"
-                placeholder="YYYY-MM-DD HH:mm"
-                className="w-72"
-                name="endTime"
-                value={formatDate(endDate)}
-                onClick={() => handleClick(false)}
-                readOnly
-              />
-            </div>
-            <div className="mt-20">
-              <Button value="Search Available Cars" />
-            </div>
-          </form>
-        </section>
-      )}
-    </>
-  )
-}
+const BookCar = () => (
+  <>
+    <Title text="BOOK CAR" />
+    <section className="font-inter text-white">
+      <Calendar label="Start Date" />
+      <Calendar label="End Date" />
+    </section>
+    <div className="mt-20">
+      <Button value="Search Available Cars" />
+    </div>
+  </>
+)
 
 export default BookCar
--- src/components/ui/Calendar.tsx
+++ src/components/ui/Calendar.tsx
@@ -1,51 +1,38 @@
-import React from "react"
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
+import dayjs from "dayjs"
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo"
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
-import { StaticDateTimePicker } from "@mui/x-date-pickers/StaticDateTimePicker"
-import Button from "@mui/material/Button"
-import { Dayjs } from "dayjs"
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
+import { MobileDateTimePicker } from "@mui/x-date-pickers/MobileDateTimePicker"
 
-interface Props {
-  setShowCalendar: (e: boolean) => void
-  currentTime: Dayjs | null
-  setCurrentTime: React.Dispatch<React.SetStateAction<Dayjs | null>>
-  onClose: (selectedTime: Dayjs | null) => void
-}
-
-const Calendar = ({
-  setShowCalendar,
-  currentTime,
-  setCurrentTime,
-  onClose,
-}: Props): React.ReactElement => {
-  const handleOkClick = () => {
-    onClose(currentTime)
+export default function Calendar({ label }: { label: string }) {
+  const pickerStyles = {
+    "& .MuiInputBase-root": {
+      color: "white",
+    },
+    "& .MuiOutlinedInput-notchedOutline": {
+      border: "none",
+    },
   }
-
-  const handleCancelClick = () => {
-    setShowCalendar(false)
-  }
-
-  const CustomBar = () => (
-    <div className="absolute bottom-0 right-0 py-5">
-      <Button onClick={handleCancelClick}>Cancel</Button>
-      <Button onClick={handleOkClick}>OK</Button>
-    </div>
-  )
-
   return (
-    <div className="relative mt-4">
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <StaticDateTimePicker
-          value={currentTime}
-          onChange={newValue => setCurrentTime(newValue)}
-          slots={{
-            actionBar: CustomBar,
-          }}
-        />
-      </LocalizationProvider>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div className={""}>
+        <DemoContainer
+          components={[
+            "DateTimePicker",
+            "MobileDateTimePicker",
+            "DesktopDateTimePicker",
+            "StaticDateTimePicker",
+          ]}
+        >
+          <DemoItem label={label}>
+            <MobileDateTimePicker
+              defaultValue={dayjs("2022-04-17T15:30")}
+              sx={pickerStyles}
+              className={"rounded-full bg-primary-200"}
+            />
+          </DemoItem>
+        </DemoContainer>
+      </div>
+    </LocalizationProvider>
   )
 }
-
-export default Calendar
