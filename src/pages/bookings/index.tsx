1e44482d62a59a461b3d6ab2d3379d2d3e7ecfa6
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-10-01 15:27:14 +0200

    feat: manage bookings

--- src/pages/bookings/index.tsx
+++ src/pages/bookings/index.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from "react"
+import Bookings from "../../components/cards/Bookings"
 
-const MyBookings = (): ReactElement => <MyBookings />
+const MyBookings = (): ReactElement => <Bookings />
 
 export default MyBookings
