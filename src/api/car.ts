c3511bddf3876f4f88d1dc905ca21191f031d833
deborah <deborahkamikazi9@gmail>
2024-10-01 10:29:52 +0200

    fix: removing empty lines

--- src/api/car.ts
+++ src/api/car.ts
@@ -1,10 +1,8 @@
 import { apiGet } from "."
-
 export const getCar = async (id: number) => {
   const { data } = await apiGet(`cars/${id}`)
   return data
 }
-
 export const getAllCars = async () => {
   const { data } = await apiGet(`cars`)
 
--- src/api/user.ts
+++ src/api/user.ts
@@ -1,10 +1,8 @@
 import { apiGet } from "."
-
 export const getUser = async (id: number) => {
   const { data } = await apiGet(`users/${id}`)
   return data
 }
-
 export const getAllUsers = async () => {
   const { data } = await apiGet(`users/`)
 
